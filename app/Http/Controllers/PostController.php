4eff0659571c1ccc9c77b038d7cfa22ddc18e029
Joel Omari <omarijoel2@gmail.com>
2021-03-19 13:13:52 +0300

    Blog post app

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $posts = Post::all();
+
+    return view('post.index')->withPosts($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('post.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //first we validate data from our form $validatedData
+        $validatedData=$this->validate($request,[
+            'title' =>'required|min:3|max:255',
+            'description' => 'required|min:3'
+        ]);
+        Post::create($validatedData); //we use create method to save a new post
+        return redirect()->route('post.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+       return view('post.show')->withPost($post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('post.edit')->withPost($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validatedData = $this->validate($request, [
+        'title'         => 'required|min:3|max:255',
+        'description'   => 'required|min:3'
+      ]);
+
+      $post->update($validatedData);
+
+      return redirect()->route('post.show', $post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post->delete();
+
+      return redirect()->route('post.index');
+    }
+}
